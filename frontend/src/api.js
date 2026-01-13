ff694728aef60530a4e6cc127793237e273cc596
prathik-shet <prathikshet20@gmail.com>
2026-01-13 10:47:46 +0530

    updated 5

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://aws-s3-upload-service-backend.onrender.com",
+  baseURL: "https://aws-s3-upload-service-backend.onrender.com/api",
   withCredentials: false,
+  timeout: 60000, // 60s safety for uploads
 });
 
 export default api;
